0ed5be8b2f8d2d879b77a8f4efa48f5ef882b1fd
rudyg <rudynoot123@gmail.com>
2025-06-09 12:18:57 -0500

    Next.js project init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Auth0Provider } from "@auth0/nextjs-auth0";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
---
+++ src/app/login/page.tsx
@@ -0,0 +1,9 @@
+
+const Login = () =>{
+    return(
+        <div className='min-h-screen w-screen bg-white'>
+
+        </div>
+    );
+}
+export default Login;
