38fdd0ceaa1c3878af6f4e8aa409a89465ec1aee
Rodolfo Gonzalez <rudynoot123@gmail.com>
2025-08-10 12:56:33 -0500

    Lambda only deploying one file

--- backend/src/functions/auth/linkedin.js
+++ backend/src/functions/auth/linkedin.js
@@ -1,22 +1,36 @@
 const axios = require('axios')
 const querystring = require('node:querystring')
 const admin = require('firebase-admin');
+const crypto = require('crypto');
 const{
     LINKEDIN_CLIENT_ID,
     LINKEDIN_CLIENT_SECRET,
     LINKEDIN_REDIRECT_URI
 } = process.env;
 
+function generateState(){
+    return crypto.randomBytes(16).toString('hex');//creates a 32 character hex string
+}
+const sessionState = generateState();
+
 exports.linkedInCodeRequest = () =>{//arrow function so its scoped to get the other secrets
+    const sessionState = generateState();
+
     const params = querystring.stringify({
         response_type:'code',
         client_id:process.env.LINKEDIN_CLIENT_ID,
-        redirect_uri:'https://yjsky4tmql.execute-api.us-east-1.amazonaws.com/auth/linkedin/callback',
-        scope:
-        //line to trigger redeploy
-        //another line lol
-        //another line syntax error
-    })
-    const linkedInAuth= 'https://www.linkedin.com/oauth/v2/authorization';
+        redirect_uri:process.env.LINKEDIN_REDIRECT_URI,
+        scope:'r_liteprofile _emailaddress',
+        state:
+    });
+    return{
+        statusCode:302,
+        headers:{
+            
+        }
+    }
+
+    
+
     
 }
