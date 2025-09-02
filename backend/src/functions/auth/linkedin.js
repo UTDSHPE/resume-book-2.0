b203e20b6773d2af6b1bb9baa03e06dd6ac6dcf3
Rodolfo Gonzalez <rudynoot123@gmail.com>
2025-09-02 15:48:59 -0500

    Redeploy

--- backend/src/functions/auth/linkedin.js
+++ backend/src/functions/auth/linkedin.js
@@ -2,7 +2,7 @@
 import crypto from 'crypto';
 import jwt from 'jsonwebtoken';
 import jwksClient from 'jwks-rsa';
-import { auth, createCustomToken, ensureAuthUser, upsertProfile } from '../auth/firebase.js';
+import { auth, createCustomToken, ensureAuthUser, upsertProfile } from './firebase.js';
 
 const {
     LINKEDIN_CLIENT_SECRET,
